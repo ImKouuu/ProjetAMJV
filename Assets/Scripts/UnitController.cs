f9e8178e11434251c53d7fb91fd9291a0c6a295e
asumamusa <kenantunc54@gmail.com>
2025-01-13 14:21:22 +0100

    arena done

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -5,15 +5,8 @@ public class UnitController : MonoBehaviour
     [SerializeField] private float health;
     [SerializeField] private float damage;
     [SerializeField] private float speed;
-
-    private DragAndDropController dragAndDropController;
-
     void Start()
     {
-        dragAndDropController = GetComponent<DragAndDropController>();
-        if (dragAndDropController == null)
-        {
-            dragAndDropController = gameObject.AddComponent<DragAndDropController>();
-        }
+
     }
 }
